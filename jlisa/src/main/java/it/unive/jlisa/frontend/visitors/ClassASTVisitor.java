5810d9b849b5d5336062b803c3cc2d107c45fab5
giacomozanatta <giacomo.zanatta@unive.it>
2025-05-08 10:12:16 +0200

    #4 Set ancestor
    Set ancestor

--- jlisa/src/main/java/it/unive/jlisa/frontend/visitors/ClassASTVisitor.java
+++ jlisa/src/main/java/it/unive/jlisa/frontend/visitors/ClassASTVisitor.java
@@ -3,8 +3,6 @@ package it.unive.jlisa.frontend.visitors;
 import it.unive.jlisa.frontend.ParserContext;
 import it.unive.jlisa.frontend.exceptions.UnsupportedStatementException;
 import it.unive.lisa.program.ClassUnit;
-import it.unive.lisa.program.InterfaceUnit;
-import it.unive.lisa.program.Program;
 import org.eclipse.jdt.core.dom.*;
 
 public class ClassASTVisitor extends JavaASTVisitor{
@@ -15,19 +13,30 @@ public class ClassASTVisitor extends JavaASTVisitor{
 
     @Override
     public boolean visit(TypeDeclaration node) {
+        ClassUnit cUnit = (ClassUnit) getProgram().getUnit(node.getName().toString());
+
         if (node.getSuperclassType() != null) {
-            throw new RuntimeException(new UnsupportedStatementException("extends is not supported yet."));
+            TypeASTVisitor visitor = new TypeASTVisitor(parserContext, source, compilationUnit);
+            node.getSuperclassType().accept(this);
+            it.unive.lisa.type.Type superType = visitor.getType();
+            if (superType != null) {
+                it.unive.lisa.program.Unit superUnit = getProgram().getUnit(superType.toString());
+                if (superUnit instanceof it.unive.lisa.program.CompilationUnit) {
+                    cUnit.addAncestor((it.unive.lisa.program.CompilationUnit)superUnit);
+                }
+            }
+            node.getSuperclassType().accept(this);
+            //throw new RuntimeException(new UnsupportedStatementException("extends is not supported yet."));
         }
         if (!node.permittedTypes().isEmpty()) {
             throw new RuntimeException(new UnsupportedStatementException("permits is not supported yet."));
         }
-        ClassUnit cUnit = (ClassUnit) getProgram().getUnit(node.getName().toString());
+
         for (FieldDeclaration fd : node.getFields()) {
             FieldDeclarationVisitor visitor = new FieldDeclarationVisitor(parserContext, source, cUnit, compilationUnit);
             fd.accept(visitor);
         }
         for (MethodDeclaration md : node.getMethods()) {
-            if (md.isConstructor()) {}
             MethodASTVisitor visitor = new MethodASTVisitor(parserContext, source, cUnit, compilationUnit);
             md.accept(visitor);
         }
