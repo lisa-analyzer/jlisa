5df388b855a0e9b200f877f98425967f18a759ea
giacomozanatta <giacomo.zanatta@unive.it>
2025-05-19 14:40:40 +0200

    #44 Add Object root class
    Add Object root class

---
+++ jlisa/src/main/java/it/unive/jlisa/program/java/constructs/object/ObjectConstructor.java
@@ -0,0 +1,63 @@
+package it.unive.jlisa.program.java.constructs.object;
+
+import it.unive.jlisa.program.java.constructs.string.StringConcat;
+import it.unive.jlisa.program.java.constructs.string.StringEndsWith;
+import it.unive.jlisa.types.JavaClassType;
+import it.unive.lisa.analysis.AbstractState;
+import it.unive.lisa.analysis.AnalysisState;
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.StatementStore;
+import it.unive.lisa.analysis.lattices.ExpressionSet;
+import it.unive.lisa.interprocedural.InterproceduralAnalysis;
+import it.unive.lisa.program.ClassUnit;
+import it.unive.lisa.program.cfg.*;
+import it.unive.lisa.program.cfg.statement.*;
+import it.unive.lisa.program.cfg.statement.string.EndsWith;
+import it.unive.lisa.program.type.BoolType;
+import it.unive.lisa.program.type.StringType;
+import it.unive.lisa.symbolic.SymbolicExpression;
+
+public class ObjectConstructor extends NativeCFG {
+
+    public ObjectConstructor(
+            CodeLocation location,
+            ClassUnit stringUnit) {
+        //JavaClassType object = JavaClassType.lookup("Object", null);
+        super(new CodeMemberDescriptor(location, stringUnit, true, "Object", JavaClassType.lookup("Object", null),
+                        new Parameter(location, "this", JavaClassType.lookup("Object", null))),
+                ObjectConstructor.JavaObjectConstructor.class);
+    }
+
+
+    public static class JavaObjectConstructor extends UnaryExpression implements PluggableStatement {
+        protected Statement originating;
+
+        public JavaObjectConstructor(CFG cfg, CodeLocation location, Expression param) {
+            super(cfg, location, "Object", param);
+        }
+
+        public static ObjectConstructor.JavaObjectConstructor build(
+                CFG cfg,
+                CodeLocation location,
+                Expression... params) {
+            return new  ObjectConstructor.JavaObjectConstructor(cfg, location, params[0]);
+        }
+
+        @Override
+        protected int compareSameClassAndParams(Statement o) {
+            return 0; // todo
+        }
+
+
+        @Override
+        public <A extends AbstractState<A>> AnalysisState<A> fwdUnarySemantics(InterproceduralAnalysis<A> interprocedural, AnalysisState<A> state, SymbolicExpression expr, StatementStore<A> expressions) throws SemanticException {
+            return state; // do nothing.
+        }
+
+        @Override
+        public void setOriginatingStatement(Statement st) {
+            originating = st;
+        }
+
+        }
+    }
