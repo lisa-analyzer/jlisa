d431f98e12be049480c5f68b4cfcf1eef01e5e90
Teodors Lisovenko <teodors.lisovenko@unive.it>
2025-05-20 08:27:49 +0200

    Added initial support for do-while loop statement
    Along with the implementation comes a test case with its input file.
    
    Closes #48

--- jlisa/src/test/java/it/unive/jlisa/LoopTests.java
+++ jlisa/src/test/java/it/unive/jlisa/LoopTests.java
@@ -9,16 +9,27 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class WhileLoopTests {
+public class LoopTests {
 
     @Test
-    public void test1() throws IOException {
+    public void testWhile1() throws IOException {
         JavaFrontend frontend = new JavaFrontend();
         List<String> inputs = new ArrayList<>();
         inputs.add("inputs/loops/while/while-1.java");
         frontend.parseFromListOfFile(inputs);
 
         LiSA lisa = TestHelpers.getLiSA("outputs/loops/while/while-1");
+        lisa.run(frontend.getProgram());
+    }
+
+    @Test
+    public void testDoWhile1() throws IOException {
+        JavaFrontend frontend = new JavaFrontend();
+        List<String> inputs = new ArrayList<>();
+        inputs.add("inputs/loops/do-while/do-while-1.java");
+        frontend.parseFromListOfFile(inputs);
+
+        LiSA lisa = TestHelpers.getLiSA("outputs/loops/do-while/do-while-1");
         lisa.run(frontend.getProgram());
     }
 }
