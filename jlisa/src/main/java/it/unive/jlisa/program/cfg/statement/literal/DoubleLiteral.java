df3ceff552a6827a19bbed25401b0df8d83aa98e
giacomozanatta <giacomo.zanatta@unive.it>
2025-06-10 15:53:10 -0400

    #11 Add numeric literals
    Add ByteLiteral, ShortLiteral, IntLiteral, LongLiteral, FloatLiteral, DoubleLiteral

---
+++ jlisa/src/main/java/it/unive/jlisa/program/cfg/statement/literal/DoubleLiteral.java
@@ -0,0 +1,15 @@
+package it.unive.jlisa.program.cfg.statement.literal;
+
+import it.unive.jlisa.program.type.DoubleType;
+import it.unive.lisa.program.cfg.CFG;
+import it.unive.lisa.program.cfg.CodeLocation;
+import it.unive.lisa.program.cfg.statement.literal.Literal;
+
+public class DoubleLiteral extends Literal<Double> {
+    public DoubleLiteral(
+            CFG cfg,
+            CodeLocation location,
+            double value) {
+        super(cfg, location, value, DoubleType.INSTANCE);
+    }
+}
---
+++ jlisa/src/main/java/it/unive/jlisa/program/cfg/statement/literal/FloatLiteral.java
@@ -0,0 +1,15 @@
+package it.unive.jlisa.program.cfg.statement.literal;
+
+import it.unive.jlisa.program.type.FloatType;
+import it.unive.lisa.program.cfg.CFG;
+import it.unive.lisa.program.cfg.CodeLocation;
+import it.unive.lisa.program.cfg.statement.literal.Literal;
+
+public class FloatLiteral extends Literal<Float> {
+    public FloatLiteral(
+            CFG cfg,
+            CodeLocation location,
+            float value) {
+        super(cfg, location, value, FloatType.INSTANCE);
+    }
+}
