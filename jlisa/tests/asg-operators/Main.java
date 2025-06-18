f4b5cf498a16d4187b433a8c4b84b68980e69a52
Vincenzo <vincenzo.arceri@unipr.it>
2025-06-18 17:41:09 +0200

    #20 Assignment operators (#65)
    * Add support for basic assignment operators #20
    
    * Some tests for assignment operators #20
    
    * Replaced Assignment with custom class JavaAssignment

---
+++ jlisa/tests/asg-operators/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	public static void main(String args[]) {
+		int a = 1;
+		int b = 2;
+		a += b; // a -> 3
+		a -= b; // a -> 1
+		a *= b + 5; // a -> 7
+		a /= b; // a -> [3, 4]
+	}
+}
