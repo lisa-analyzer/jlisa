ebe6789f352f5c72206ada9f2c8d8e27ff61d2b4
filippo_bianchi <fillobia@gmail.com>
2025-06-20 12:46:13 +0200

    Class static type is modeled as a reference type

--- jlisa/src/main/java/it/unive/jlisa/frontend/visitors/StatementASTVisitor.java
+++ jlisa/src/main/java/it/unive/jlisa/frontend/visitors/StatementASTVisitor.java
@@ -21,6 +21,7 @@ import it.unive.lisa.program.cfg.statement.call.UnresolvedCall;
 import it.unive.lisa.program.cfg.statement.literal.Int32Literal;
 import it.unive.lisa.program.cfg.statement.literal.NullLiteral;
 import it.unive.lisa.program.type.Int32Type;
+import it.unive.lisa.type.ReferenceType;
 import it.unive.lisa.type.Type;
 import it.unive.lisa.util.datastructures.graph.code.NodeList;
 import org.eclipse.jdt.core.dom.*;
@@ -400,6 +401,9 @@ public class StatementASTVisitor extends JavaASTVisitor {
         TypeASTVisitor visitor = new TypeASTVisitor(this.parserContext, source, compilationUnit);
         node.getType().accept(visitor);
         Type variableType = visitor.getType();
+        if (variableType.isInMemoryType()) {
+        	variableType = new ReferenceType(variableType);
+        }
         for (Object f : node.fragments()) {
             VariableDeclarationFragment fragment = (VariableDeclarationFragment) f;
             String variableName = fragment.getName().getIdentifier();
