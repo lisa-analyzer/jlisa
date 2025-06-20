de116623ce28a00931dc8fa8f0d1bde878fb3acc
VincenzoArceri <vincenzoarceri.92@gmail.com>
2025-06-20 22:43:08 +0200

    Simplified Java assignment (relying now on runtime types of the right-hand expression)

--- jlisa/src/main/java/it/unive/jlisa/program/cfg/statement/JavaAssignment.java
+++ jlisa/src/main/java/it/unive/jlisa/program/cfg/statement/JavaAssignment.java
@@ -14,42 +14,43 @@ import it.unive.lisa.symbolic.SymbolicExpression;
 import it.unive.lisa.symbolic.value.BinaryExpression;
 import it.unive.lisa.symbolic.value.Constant;
 import it.unive.lisa.symbolic.value.operator.binary.TypeConv;
+import it.unive.lisa.type.Type;
 import it.unive.lisa.type.TypeTokenType;
 
 import java.util.Collections;
+import java.util.Set;
 
 public class JavaAssignment extends Assignment {
 
-    public JavaAssignment(
-            CFG cfg,
-            CodeLocation location,
-            Expression target,
-            Expression expression) {
-        super(cfg, location, RightToLeftEvaluation.INSTANCE, target.getStaticType(), target, expression);
-    }
+	public JavaAssignment(
+			CFG cfg,
+			CodeLocation location,
+			Expression target,
+			Expression expression) {
+		super(cfg, location, RightToLeftEvaluation.INSTANCE, target.getStaticType(), target, expression);
+	}
 
-    @Override
-    public <A extends AbstractState<A>> AnalysisState<A> fwdBinarySemantics(
-            InterproceduralAnalysis<A> interprocedural,
-            AnalysisState<A> state,
-            SymbolicExpression left,
-            SymbolicExpression right,
-            StatementStore<A> expressions)
-            throws SemanticException {
-        CodeLocation loc = getLocation();
-        if (!right.getStaticType().equals(left.getStaticType())) {
-        	if (right.getStaticType().isUntyped()) {
-                return super.fwdBinarySemantics(interprocedural, state, left, right, expressions);
-            } else if (right.getStaticType().canBeAssignedTo(left.getStaticType())) {
-                 Constant typeCast = new Constant(new TypeTokenType(Collections.singleton(left.getStaticType())), left.getStaticType(), loc);
+	@Override
+	public <A extends AbstractState<A>> AnalysisState<A> fwdBinarySemantics(
+			InterproceduralAnalysis<A> interprocedural,
+			AnalysisState<A> state,
+			SymbolicExpression left,
+			SymbolicExpression right,
+			StatementStore<A> expressions)
+					throws SemanticException {
+		CodeLocation loc = getLocation();
+		AnalysisState<A> result = state.bottom();
+		Set<Type> rightTypes = state.getState().getRuntimeTypesOf(right, this, state.getState());
 
-                 BinaryExpression castExpression =  new BinaryExpression(left.getStaticType(), right, typeCast, TypeConv.INSTANCE, loc);
-                 return super.fwdBinarySemantics(interprocedural, state, left, castExpression, expressions);
-             } else {
-                return state.bottom();
-            }
-        }
-
-        return super.fwdBinarySemantics(interprocedural, state, left, right, expressions);
-    }
+		for (Type rType : rightTypes) {
+			if (rType.equals(left.getStaticType()))
+				result = result.lub(super.fwdBinarySemantics(interprocedural, state, left, right, expressions));
+			else if (rType.canBeAssignedTo(left.getStaticType())) {
+				Constant typeConv = new Constant(new TypeTokenType(Collections.singleton(left.getStaticType())), left.getStaticType(), loc);
+				BinaryExpression castExpression =  new BinaryExpression(left.getStaticType(), right, typeConv, TypeConv.INSTANCE, loc);
+				result = result.lub(super.fwdBinarySemantics(interprocedural, state, left, castExpression, expressions));
+			}
+		}
+		return result;
+	}
 }
