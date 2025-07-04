7e635de396d382558aebd18866d041730aea641a
VincenzoArceri <vincenzoarceri.92@gmail.com>
2025-07-04 08:42:04 +0200

    Field type if in memory type is changed in reference type

--- jlisa/src/main/java/it/unive/jlisa/frontend/visitors/FieldDeclarationVisitor.java
+++ jlisa/src/main/java/it/unive/jlisa/frontend/visitors/FieldDeclarationVisitor.java
@@ -1,71 +1,56 @@
 package it.unive.jlisa.frontend.visitors;
 
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.FieldDeclaration;
+import org.eclipse.jdt.core.dom.Modifier;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
+
 import it.unive.jlisa.frontend.ParserContext;
 import it.unive.jlisa.program.type.JavaArrayType;
-import it.unive.jlisa.program.type.JavaClassType;
-import it.unive.jlisa.type.JavaTypeSystem;
 import it.unive.lisa.program.Global;
-import it.unive.lisa.program.Program;
-import it.unive.lisa.program.SourceCodeLocation;
 import it.unive.lisa.program.annotations.Annotations;
-import it.unive.lisa.program.cfg.CFG;
-import it.unive.lisa.program.cfg.CodeMemberDescriptor;
-import it.unive.lisa.program.cfg.Parameter;
-import it.unive.lisa.program.cfg.edge.SequentialEdge;
-import it.unive.lisa.program.cfg.statement.Ret;
-import it.unive.lisa.program.cfg.statement.VariableRef;
-import it.unive.lisa.program.cfg.statement.global.AccessInstanceGlobal;
 import it.unive.lisa.type.ArrayType;
 import it.unive.lisa.type.ReferenceType;
 import it.unive.lisa.type.Type;
-import it.unive.lisa.type.Untyped;
-import org.eclipse.jdt.core.dom.*;
-
-import javax.lang.model.type.TypeVisitor;
-import java.util.ArrayList;
-import java.util.List;
 
 public class FieldDeclarationVisitor extends JavaASTVisitor {
-    it.unive.lisa.program.CompilationUnit lisacompilationUnit;
+	it.unive.lisa.program.CompilationUnit lisacompilationUnit;
 
-    public FieldDeclarationVisitor(ParserContext parserContext, String source, it.unive.lisa.program.CompilationUnit lisacompilationUnit, CompilationUnit astCompilationUnit) {
-        super(parserContext, source, astCompilationUnit);
-        this.lisacompilationUnit = lisacompilationUnit;
-    }
-    @Override
-    public boolean visit(FieldDeclaration node) {
-        int modifiers = node.getModifiers();
-        TypeASTVisitor typeVisitor = new TypeASTVisitor(parserContext, source, compilationUnit);
-        node.getType().accept(typeVisitor);
-        Type type = typeVisitor.getType();
+	public FieldDeclarationVisitor(ParserContext parserContext, String source, it.unive.lisa.program.CompilationUnit lisacompilationUnit, CompilationUnit astCompilationUnit) {
+		super(parserContext, source, astCompilationUnit);
+		this.lisacompilationUnit = lisacompilationUnit;
+	}
+	@Override
+	public boolean visit(FieldDeclaration node) {
+		int modifiers = node.getModifiers();
+		TypeASTVisitor typeVisitor = new TypeASTVisitor(parserContext, source, compilationUnit);
+		node.getType().accept(typeVisitor);
+		Type type = typeVisitor.getType();
+		if (type.isInMemoryType())
+			type = new ReferenceType(type);
 
+		for (Object f : node.fragments()) {
+			VariableDeclarationFragment fragment = (VariableDeclarationFragment) f;
+			if (fragment.getExtraDimensions() != 0) {
+				if (type instanceof ArrayType) {
+					ArrayType arrayType = (ArrayType) type;
+					int dim = arrayType.getDimensions();
+					type = JavaArrayType.lookup(arrayType.getBaseType(), dim + fragment.getExtraDimensions());
+				} else {
+					type = JavaArrayType.lookup(type, fragment.getExtraDimensions());
+				}
+			}
+			String identifier = fragment.getName().getIdentifier();
 
-        for (Object f : node.fragments()) {
-            VariableDeclarationFragment fragment = (VariableDeclarationFragment) f;
-            String variableName = fragment.getName().getIdentifier();
-            SourceCodeLocation loc = getSourceCodeLocation(fragment);
-            if (fragment.getExtraDimensions() != 0) {
-                if (type instanceof ArrayType) {
-                    ArrayType arrayType = (ArrayType) type;
-                    int dim = arrayType.getDimensions();
-                    type = JavaArrayType.lookup(arrayType.getBaseType(), dim + fragment.getExtraDimensions());
-                } else {
-                    type = JavaArrayType.lookup(type, fragment.getExtraDimensions());
-                }
-            }
-            it.unive.lisa.program.cfg.statement.Expression initializer = null;
+			boolean isStatic = Modifier.isStatic(modifiers);
+			Global global = new Global(getSourceCodeLocation(node), lisacompilationUnit, identifier, !isStatic, type, new Annotations());
+			if (isStatic) {
+				lisacompilationUnit.addGlobal(global);
+			} else {
+				lisacompilationUnit.addInstanceGlobal(global);
+			}
+		}
 
-            String identifier = fragment.getName().getIdentifier();
-
-            boolean isStatic = Modifier.isStatic(modifiers);
-            Global global = new Global(getSourceCodeLocation(node), lisacompilationUnit, identifier, !isStatic, type, new Annotations());
-            if (isStatic) {
-                lisacompilationUnit.addGlobal(global);
-            } else {
-                lisacompilationUnit.addInstanceGlobal(global);
-            }
-        }
-
-        return false;
-    }
+		return false;
+	}
 }
