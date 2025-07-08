06f9f063122343bddb19285d72586f7a75d5057f
VincenzoArceri <vincenzoarceri.92@gmail.com>
2025-07-08 17:00:41 +0200

    add charValue method

--- jlisa/java-testcases/character/Main.java
+++ jlisa/java-testcases/character/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 	
     public static void main(String[] args) {
-    	Character c = new Character('a');  	
+    	Character c = new Character('a');  
+    	char d = c.charValue();
     }
 }
