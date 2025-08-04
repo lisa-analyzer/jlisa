f24530f5e0e50df4e4c0e9a9759e42fd1bc7aa3f
filippo_bianchi <fillobia@gmail.com>
2025-08-04 13:09:57 +0200

    Added default values for JavaDoubleType and JavaFloatType

--- jlisa/java-testcases/array-with-initializer/Main.java
+++ jlisa/java-testcases/array-with-initializer/Main.java
@@ -3,7 +3,12 @@ public class Main{
 	public static void main(String[] args) {
 
 		int[] a = new int[] {1,2,3};
-			
+	
+		/* TODO wait for floating point support
+		float[] b = new float[] {1.2f, 3.5f, 0f};
+		
+		double[] c = new double[] {6.5, 9.8, 0};
+		*/
 	}
 	
 }
--- jlisa/java-testcases/array/Main.java
+++ jlisa/java-testcases/array/Main.java
@@ -2,5 +2,12 @@ public class Main {
 	
     public static void main(String[] args) {
     	int[] a = new int[10];
+    	
+    	/* TODO wait for floating point support
+    	float[] b = new float[3];
+    	
+    	double[] c = new double[5];
+    	*/    	
+    	   
     }
 }
