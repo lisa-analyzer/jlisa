45fcdd29636356a8b7400ab925297cf81a35e992
filippo_bianchi <fillobia@gmail.com>
2025-08-10 16:51:49 +0200

    Added array access semantics and updated tests

--- jlisa/java-testcases/array-with-initializer/Main.java
+++ jlisa/java-testcases/array-with-initializer/Main.java
@@ -3,6 +3,12 @@ public class Main{
 	public static void main(String[] args) {
 
 		int[] a = new int[] {1,2,3};
+		
+		int d = a[2];
+		
+		int[] f = new int[] {9,2,5,6,7,4,8,1};
+		
+		d = f[3];
 	
 		/* TODO wait for floating point support
 		float[] b = new float[] {1.2f, 3.5f, 0f};
--- jlisa/java-testcases/array/Main.java
+++ jlisa/java-testcases/array/Main.java
@@ -3,6 +3,10 @@ public class Main {
     public static void main(String[] args) {
     	int[] a = new int[10];
     	
+    	int b = a[9];
+    	
+    	//a[3] = 4;
+    	
     	/* TODO wait for floating point support
     	float[] b = new float[3];
     	
