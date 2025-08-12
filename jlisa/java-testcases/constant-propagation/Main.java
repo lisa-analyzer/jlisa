eda333e2d770a6c612dcab61aed1d281c2fe9a0f
filippo_bianchi <fillobia@gmail.com>
2025-08-12 18:29:10 +0200

    Added multiplication and division for constant propagation.

---
+++ jlisa/java-testcases/constant-propagation/Main.java
@@ -0,0 +1,33 @@
+public class Main{
+	
+	public static void main(String[] args) {
+	
+		int a = 6;
+		int b = a + 3;
+		int c = a - 4;
+		int d = a * 6;
+		int e = a / 2;
+		
+		int p = (2 * a) + ((a + 3) - 5) * a;
+		
+		
+		float f = 7.5f;
+		float g = f + 3.5f;
+		float h = f - 2.1f;
+		float i = f * 0.3f;
+		float j = f / 2.2f;
+		
+		
+		double k = 4.2;
+		double l = k + 3.2;
+		double m = k - 2.0;
+		double n = k * 4;
+		double o = k / 1.5;
+			
+		String w = "Hello";
+		
+		String x = w + " World!";
+		
+	}
+	
+}
