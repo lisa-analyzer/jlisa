361a5cbf034e2bc51a4f84fc1a1559524383650a
VincenzoArceri <vincenzoarceri.92@gmail.com>
2025-08-13 23:22:57 +0200

    AssertionError class with empty constructor

---
+++ jlisa/src/main/java/it/unive/jlisa/program/java/constructs/assertionerror/AssertionErrorEmptyConstructor.java
@@ -0,0 +1,48 @@
+package it.unive.jlisa.program.java.constructs.assertionerror;
+
+import it.unive.lisa.analysis.AbstractDomain;
+import it.unive.lisa.analysis.AbstractLattice;
+import it.unive.lisa.analysis.AnalysisState;
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.StatementStore;
+import it.unive.lisa.analysis.lattices.ExpressionSet;
+import it.unive.lisa.interprocedural.InterproceduralAnalysis;
+import it.unive.lisa.program.cfg.CFG;
+import it.unive.lisa.program.cfg.CodeLocation;
+import it.unive.lisa.program.cfg.statement.Expression;
+import it.unive.lisa.program.cfg.statement.NaryExpression;
+import it.unive.lisa.program.cfg.statement.PluggableStatement;
+import it.unive.lisa.program.cfg.statement.Statement;
+import it.unive.lisa.symbolic.value.PushAny;
+
+public class AssertionErrorEmptyConstructor extends NaryExpression implements PluggableStatement {
+	protected Statement originating;
+
+	public AssertionErrorEmptyConstructor(CFG cfg, CodeLocation location) {
+		super(cfg, location, "AssertionError");
+	}
+
+	public static AssertionErrorEmptyConstructor build(
+			CFG cfg,
+			CodeLocation location,
+			Expression... params) {
+		return new AssertionErrorEmptyConstructor(cfg, location);
+	}
+
+	@Override
+	protected int compareSameClassAndParams(Statement o) {
+		return 0; 
+	}
+	
+	@Override
+	public void setOriginatingStatement(Statement st) {
+		originating = st;
+	}
+
+	@Override
+	public <A extends AbstractLattice<A>,
+		D extends AbstractDomain<A>> AnalysisState<A> forwardSemanticsAux(InterproceduralAnalysis<A, D> interprocedural,
+			AnalysisState<A> state, ExpressionSet[] params, StatementStore<A> expressions) throws SemanticException {
+		return interprocedural.getAnalysis().smallStepSemantics(state, new PushAny(getStaticType(), getLocation()), originating);
+	}
+}
