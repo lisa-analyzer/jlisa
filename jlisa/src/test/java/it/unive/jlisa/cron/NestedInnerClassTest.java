6a7331b3be7d569042247f58113f44cdfbb5fbeb
olivieriluca <oli.luca93@gmail.com>
2025-08-28 09:59:22 +0200

    Regenerated nested inner class test case

--- jlisa/src/test/java/it/unive/jlisa/cron/NestedInnerClassTest.java
+++ jlisa/src/test/java/it/unive/jlisa/cron/NestedInnerClassTest.java
@@ -11,21 +11,9 @@ import it.unive.lisa.conf.LiSAConfiguration.GraphType;
 
 public class NestedInnerClassTest extends JLiSAAnalysisExecutor {
 
-	private static CronConfiguration createConfiguration(String testDir, String subDir, String... programFiles) {
-		CronConfiguration configuration = TestHelpers.createConfiguration(testDir, subDir, programFiles);
-
-		configuration.serializeInputs = true;
-		configuration.serializeResults = false;
-		configuration.jsonOutput = true;
-		configuration.analysis = null;
-		configuration.analysisGraphs = GraphType.NONE;
-		return configuration;
-	}
-
-    
 	@Test
 	public void testNestedInnerClass1()  throws IOException {
-		CronConfiguration conf = createConfiguration("nested-inner-class", "nest-1", "nest-1.java");
+		CronConfiguration conf = TestHelpers.constantPropagation("nested-inner-class", "nest-1", "nest-1.java");
 		perform(conf);
 	}
 	
