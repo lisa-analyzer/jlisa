c06fe9dde3542c3f17f51cacef7bb63c574c4255
filippo_bianchi <fillobia@gmail.com>
2025-08-28 15:31:21 +0200

    First draft string getBytes

---
+++ jlisa/src/main/java/it/unive/jlisa/program/java/constructs/string/StringGetBytes.java
@@ -0,0 +1,69 @@
+package it.unive.jlisa.program.java.constructs.string;
+
+import it.unive.jlisa.program.operator.JavaStringGetBytesOperator;
+import it.unive.lisa.analysis.AbstractDomain;
+import it.unive.lisa.analysis.AbstractLattice;
+import it.unive.lisa.analysis.AnalysisState;
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.StatementStore;
+import it.unive.lisa.interprocedural.InterproceduralAnalysis;
+import it.unive.lisa.program.cfg.CFG;
+import it.unive.lisa.program.cfg.CodeLocation;
+import it.unive.lisa.program.cfg.statement.Expression;
+import it.unive.lisa.program.cfg.statement.PluggableStatement;
+import it.unive.lisa.program.cfg.statement.Statement;
+import it.unive.lisa.program.cfg.statement.UnaryExpression;
+import it.unive.lisa.symbolic.SymbolicExpression;
+import it.unive.lisa.symbolic.heap.AccessChild;
+import it.unive.lisa.symbolic.heap.HeapDereference;
+import it.unive.lisa.symbolic.value.GlobalVariable;
+import it.unive.lisa.type.Type;
+import it.unive.lisa.type.Untyped;
+
+public class StringGetBytes extends UnaryExpression implements PluggableStatement {
+	protected Statement originating;
+
+	public StringGetBytes(CFG cfg, CodeLocation location, Expression exp) {
+		super(cfg, location, "valueOf", exp);
+	}
+	
+	public static StringGetBytes build(
+			CFG cfg,
+			CodeLocation location,
+			Expression... params) {
+		return new StringGetBytes(cfg, location, params[0]);
+	}
+	
+	@Override
+	protected int compareSameClassAndParams(Statement o) {
+		return 0; 
+	}
+
+
+	@Override
+	public void setOriginatingStatement(Statement st) {
+		originating = st;
+	}
+
+	@Override
+	public <A extends AbstractLattice<A>, D extends AbstractDomain<A>> AnalysisState<A> fwdUnarySemantics(
+			InterproceduralAnalysis<A, D> interprocedural, AnalysisState<A> state, SymbolicExpression expr,
+			StatementStore<A> expressions) throws SemanticException {
+		Type stringType = getProgram().getTypes().getStringType();
+		GlobalVariable var = new GlobalVariable(Untyped.INSTANCE, "value", getLocation());
+		
+		HeapDereference deref = new HeapDereference(stringType, expr, getLocation());
+		AccessChild accessExpr = new AccessChild(stringType, deref, var, getLocation());
+		
+		it.unive.lisa.symbolic.value.UnaryExpression getBytes  = new it.unive.lisa.symbolic.value.UnaryExpression(
+				stringType, 
+				accessExpr, 
+				JavaStringGetBytesOperator.INSTANCE, 
+				getLocation());
+		
+		
+		return interprocedural.getAnalysis().smallStepSemantics(state, getBytes, originating);
+
+		
+	}
+}
---
+++ jlisa/src/main/java/it/unive/jlisa/program/operator/JavaStringGetBytesOperator.java
@@ -0,0 +1,35 @@
+package it.unive.jlisa.program.operator;
+
+import java.util.Collections;
+import java.util.Set;
+
+import it.unive.lisa.symbolic.value.operator.StringOperator;
+import it.unive.lisa.symbolic.value.operator.unary.UnaryOperator;
+import it.unive.lisa.type.Type;
+import it.unive.lisa.type.TypeSystem;
+
+public class JavaStringGetBytesOperator implements StringOperator, UnaryOperator {
+
+	/**
+	 * The singleton instance of this class.
+	 */
+	public static final JavaStringGetBytesOperator INSTANCE = new JavaStringGetBytesOperator();
+
+	/**
+	 * Builds the operator. This constructor is visible to allow subclassing:
+	 * instances of this class should be unique, and the singleton can be
+	 * retrieved through field {@link #INSTANCE}.
+	 */
+	protected JavaStringGetBytesOperator() {
+	}
+
+	@Override
+	public String toString() {
+		return "strvalueof";
+	}
+		
+	@Override
+	public Set<Type> typeInference(TypeSystem types, Set<Type> argument) {
+		return Collections.singleton(types.getType("JavaArrayType"));
+	}
+}
