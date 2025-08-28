7858afd51ca28711bf3af8e8466d945d90847df8
VincenzoArceri <vincenzoarceri.92@gmail.com>
2025-08-28 10:14:12 +0200

    Fixing nested test

--- jlisa/src/test/java/it/unive/jlisa/cron/NestedInnerClassTest.java
+++ jlisa/src/test/java/it/unive/jlisa/cron/NestedInnerClassTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import it.unive.jlisa.helpers.CronConfiguration;
 import it.unive.jlisa.helpers.JLiSAAnalysisExecutor;
 import it.unive.jlisa.helpers.TestHelpers;
-import it.unive.lisa.conf.LiSAConfiguration.GraphType;
 
 public class NestedInnerClassTest extends JLiSAAnalysisExecutor {
 
