128360985725b7ce06f6ec0c9209949e993235df
VincenzoArceri <vincenzoarceri.92@gmail.com>
2025-08-29 16:05:58 +0200

    Renaming

---
+++ jlisa/src/test/java/it/unive/jlisa/cron/MethodInvocationTest.java
@@ -0,0 +1,30 @@
+package it.unive.jlisa.cron;
+
+import it.unive.jlisa.helpers.CronConfiguration;
+import it.unive.jlisa.helpers.JLiSAAnalysisExecutor;
+import it.unive.jlisa.helpers.TestHelpers;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class MethodInvocationTest  extends JLiSAAnalysisExecutor {
+
+    @Test
+    public void test01()  throws IOException {
+        CronConfiguration conf = TestHelpers.createConfiguration("method-invocation", "test1", "Main.java", "A.java", "B.java");
+        perform(conf);
+    }
+
+    @Test
+    public void test02()  throws IOException {
+        CronConfiguration conf = TestHelpers.createConfiguration("method-invocation", "test2", "Main.java", "A.java", "B.java", "C.java");
+        perform(conf);
+    }
+
+    @Test
+    public void test03()  throws IOException {
+        CronConfiguration conf = TestHelpers.createConfiguration("method-invocation", "test3", "Main.java", "A.java", "B.java", "C.java");
+        perform(conf);
+    }
+
+}
