e6f5fc774a1e93efe8342eefce1aae5f0fc91e8d
VincenzoArceri <vincenzoarceri.92@gmail.com>
2025-09-02 09:55:56 +0200

    Added test for class init

---
+++ jlisa/java-testcases/class-init/class-init01/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {   
+	  MyClass cl = new MyClass();
+	  
+	  String s = MyClass.MYSTRING;
+	  int i = MyClass.MYINT;
+	  
+  }
+}
---
+++ jlisa/src/test/java/it/unive/jlisa/cron/ClassInitializationTest.java
@@ -0,0 +1,18 @@
+package it.unive.jlisa.cron;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import it.unive.jlisa.helpers.CronConfiguration;
+import it.unive.jlisa.helpers.JLiSAAnalysisExecutor;
+import it.unive.jlisa.helpers.TestHelpers;
+
+public class ClassInitializationTest extends JLiSAAnalysisExecutor {
+
+	@Test
+	public void tesClassInit01()  throws IOException {
+		CronConfiguration conf = TestHelpers.constantPropagation("class-init", "class-init01", "Main.java", "MyClass.java");
+		perform(conf);
+	}
+}
