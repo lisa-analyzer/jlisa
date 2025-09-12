5a9f8480fc806e5f17c8a83a0068963f73276444
VincenzoArceri <vincenzoarceri.92@gmail.com>
2025-09-12 14:01:52 +0200

    SVComp tests

--- jlisa/src/test/java/it/unive/jlisa/svcomp/SVCompTestcases.java
+++ jlisa/src/test/java/it/unive/jlisa/svcomp/SVCompTestcases.java
@@ -197,6 +197,14 @@ public class SVCompTestcases extends JLiSAAnalysisExecutor {
 		perform(conf);
 	}
 	
+	@Test
+	public void StringContains02_test()  throws IOException {
+		CronConfiguration conf = TestHelpers.assertCheckerWithConstantPropagation("svcomp", "StringContains02", 
+				"Main.java",
+				"Verifier.java");
+		perform(conf);
+	}
+	
 	// FIXME: need equalsIgnoreCase and regionMatches
 	@Test
 	public void StringCompare01_test()  throws IOException {
