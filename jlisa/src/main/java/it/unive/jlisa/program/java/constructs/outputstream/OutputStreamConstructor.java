abed970aec4c42031aac50bb2bff3f1b19bbdc3f
VincenzoArceri <vincenzoarceri.92@gmail.com>
2025-09-13 18:40:18 +0200

    Adding OutputStream constructor

---
+++ jlisa/src/main/java/it/unive/jlisa/program/java/constructs/outputstream/OutputStreamConstructor.java
@@ -0,0 +1,54 @@
+package it.unive.jlisa.program.java.constructs.outputstream;
+
+import it.unive.jlisa.program.type.JavaClassType;
+import it.unive.lisa.analysis.*;
+import it.unive.lisa.analysis.lattices.ExpressionSet;
+import it.unive.lisa.interprocedural.InterproceduralAnalysis;
+import it.unive.lisa.program.cfg.CFG;
+import it.unive.lisa.program.cfg.CodeLocation;
+import it.unive.lisa.program.cfg.statement.Expression;
+import it.unive.lisa.program.cfg.statement.NaryExpression;
+import it.unive.lisa.program.cfg.statement.PluggableStatement;
+import it.unive.lisa.program.cfg.statement.Statement;
+import it.unive.lisa.symbolic.value.PushAny;
+
+public class OutputStreamConstructor extends NaryExpression implements PluggableStatement {
+	protected Statement originating;
+
+	public OutputStreamConstructor(
+			CFG cfg,
+			CodeLocation location) {
+		super(cfg, location, "OutputStream", JavaClassType.lookup("OutputStream", null));
+	}
+
+	public static OutputStreamConstructor build(
+			CFG cfg,
+			CodeLocation location,
+			Expression... params) {
+		return new OutputStreamConstructor(cfg, location);
+	}
+
+	@Override
+	protected int compareSameClassAndParams(
+			Statement o) {
+		return 0;
+	}
+
+	@Override
+	public void setOriginatingStatement(
+			Statement st) {
+		originating = st;
+	}
+
+	@Override
+	public <A extends AbstractLattice<A>,
+			D extends AbstractDomain<A>> AnalysisState<A> forwardSemanticsAux(
+					InterproceduralAnalysis<A, D> interprocedural,
+					AnalysisState<A> state,
+					ExpressionSet[] params,
+					StatementStore<A> expressions)
+					throws SemanticException {
+		return interprocedural.getAnalysis().smallStepSemantics(state, new PushAny(getStaticType(), getLocation()),
+				originating);
+	}
+}
