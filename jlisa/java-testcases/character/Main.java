2f3414faa3cc130e80013e0d2443c633a650b045
giacomozanatta <giacomo.zanatta@unive.it>
2025-09-14 16:06:10 -0400

    Fix Character.java

--- jlisa/java-testcases/character/Main.java
+++ jlisa/java-testcases/character/Main.java
@@ -3,7 +3,7 @@ public class Main{
 	public static void main(String[] args) {
 		
 		Character c1 = new Character('a');
-		//Character c2 = Character.valueOf('a');
+		Character c2 = Character.valueOf('a');
 		boolean b1 = Character.isDigit('0');
 		boolean b2 = Character.isDefined('a');
 		char char1 = Character.forDigit(2, 10);
