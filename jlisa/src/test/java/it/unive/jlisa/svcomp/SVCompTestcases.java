91ceebacd82840766df18a83ddc69ff172895854
VincenzoArceri <vincenzoarceri.92@gmail.com>
2025-09-15 21:13:27 +0200

    Setting source of unresolved static call, test for SVComp

--- jlisa/src/test/java/it/unive/jlisa/svcomp/SVCompTestcases.java
+++ jlisa/src/test/java/it/unive/jlisa/svcomp/SVCompTestcases.java
@@ -250,6 +250,14 @@ public class SVCompTestcases extends JLiSAAnalysisExecutor {
 		perform(conf);
 	}
 
+	@Test
+	public void exceptions9_test() throws IOException {
+		CronConfiguration conf = TestHelpers.assertCheckerWithConstantPropagation("svcomp", "exceptions9",
+				"Main.java",
+				"Verifier.java");
+		perform(conf);
+	}
+
 	// FIXME: need equalsIgnoreCase and regionMatches
 	@Test
 	public void StringCompare01_test() throws IOException {
