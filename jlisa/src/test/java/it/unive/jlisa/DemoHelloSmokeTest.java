237d23ffa6a003dbaf1ece178cb1863490847380
delarasa <delarasalimiii@gmail.com>
2025-10-06 16:27:19 +0200

    feat(demo): add Hello demo (DemoApplication + Runner) and run instructions

--- jlisa/src/test/java/it/unive/jlisa/DemoHelloSmokeTest.java
+++ jlisa/src/test/java/it/unive/jlisa/DemoHelloSmokeTest.java
@@ -1,2 +1,26 @@
-package it.unive.jlisa;public class DemoHelloSmokeTest {
+package it.unive.jlisa;
+
+import it.unive.jlisa.helpers.CronConfiguration;
+import it.unive.jlisa.helpers.JLiSAAnalysisExecutor;
+import it.unive.jlisa.helpers.TestHelpers;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class DemoHelloSmokeTest {
+
+    @Test
+    public void runOnDemoApplication() throws IOException {
+
+        CronConfiguration conf = TestHelpers.constantPropagation(
+                "src/test/resources",             // testDir
+                "demo/com/example/demo",          // subDir
+                "DemoApplication.java"            // program file
+        );
+        conf.workdir = "tests-output/demo-hello";
+        conf.serializeResults = true;
+
+
+        JLiSAAnalysisExecutor().perform(conf);
+    }
 }
