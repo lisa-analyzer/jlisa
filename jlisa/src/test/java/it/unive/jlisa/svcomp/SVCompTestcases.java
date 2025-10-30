f9b780b7fe2865bdd523fd31d1cbb66d9ac741ee
VincenzoArceri <vincenzoarceri.92@gmail.com>
2025-10-30 16:22:51 +0100

    Refactoring split semantics, updating tests

--- jlisa/src/test/java/it/unive/jlisa/svcomp/SVCompTestcases.java
+++ jlisa/src/test/java/it/unive/jlisa/svcomp/SVCompTestcases.java
@@ -614,6 +614,14 @@ public class SVCompTestcases extends JLiSAAnalysisExecutor {
 		perform(conf);
 	}
 
+	@Test
+	public void RegexSubstitution01_test() throws IOException {
+		CronConfiguration conf = TestHelpers.assertCheckerWithConstantPropagation("svcomp",
+				"RegexSubstitution01",
+				"Main.java", "../common");
+		perform(conf);
+	}
+
 	@Ignore
 	public void BellmanFord_MemUnsat01_test() throws IOException {
 		CronConfiguration conf = TestHelpers.assertCheckerWithConstantPropagation("svcomp",
