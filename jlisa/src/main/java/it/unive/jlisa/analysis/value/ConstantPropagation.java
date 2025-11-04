6dfc24f30ac09944d0e848c2d765ad2e69c344e0
filippo_bianchi <fillobia@gmail.com>
2025-11-04 16:36:36 +0100

    Added long toOctalString and updated tests

--- jlisa/src/main/java/it/unive/jlisa/analysis/value/ConstantPropagation.java
+++ jlisa/src/main/java/it/unive/jlisa/analysis/value/ConstantPropagation.java
@@ -336,6 +336,10 @@ public class ConstantPropagation implements BaseNonRelationalValueDomain<Constan
 			if (arg.getValue() instanceof Long l)
 				return new ConstantValue(Long.numberOfTrailingZeros(l));
 		
+		if (operator instanceof JavaLongToOctalStringOperator)
+			if (arg.getValue() instanceof Long l)
+				return new ConstantValue(Long.toOctalString(l));
+		
 		// strings
 		if (operator instanceof JavaStringLengthOperator && arg.getValue() instanceof String str)
 			return new ConstantValue(str.length());
