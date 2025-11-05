2134e4eb3e6857a9980a6004e6bf33b4ce017b6e
giacomozanatta <giacomo.zanatta@unive.it>
2025-11-05 18:18:32 +0100

    Update tests

--- jlisa/src/test/java/it/unive/jlisa/cron/ByteTest.java
+++ jlisa/src/test/java/it/unive/jlisa/cron/ByteTest.java
@@ -3,7 +3,6 @@ package it.unive.jlisa.cron;
 import it.unive.jlisa.helpers.CronConfiguration;
 import it.unive.jlisa.helpers.JLiSAAnalysisExecutor;
 import it.unive.jlisa.helpers.TestHelpers;
-import it.unive.lisa.conf.LiSAConfiguration;
 import java.io.IOException;
 import org.junit.jupiter.api.Test;
 
@@ -12,7 +11,6 @@ public class ByteTest extends JLiSAAnalysisExecutor {
 	@Test
 	public void testByte() throws IOException {
 		CronConfiguration conf = TestHelpers.constantPropagation("byte", "", "ByteTest.java");
-		conf.analysisGraphs = LiSAConfiguration.GraphType.HTML_WITH_SUBNODES;
 		perform(conf);
 	}
 
