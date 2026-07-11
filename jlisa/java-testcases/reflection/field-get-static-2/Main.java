5a21147eed367579c80a37bddc42c3ed0ca571c1
= <karimouhamou@gmail.com>
2026-07-11 17:16:11 +0200

    test: add inherited static field and non-null get to static field tests

--- jlisa/java-testcases/reflection/field-get-static-2/Main.java
+++ jlisa/java-testcases/reflection/field-get-static-2/Main.java
@@ -27,14 +27,30 @@ public class Main {
 		// Integer wrapper static field
 		Field fw = c.getField("staticWrapper");
 		Object vw = fw.get(null);
+
+		// static field inherited from superclass
+		Class childC = Class.forName("Child");
+		Field fsuper = childC.getField("parentStatic");
+		Object vsuper = fsuper.get(null);
+
+		// static field .get() with non-null argument (should be ignored)
+		Holder instance = new Holder();
+		Object vNonNull = fi.get(instance);
 	}
 }
 
-class Holder {
+class Base {
+	public static int parentStatic = 77;
+}
+
+class Holder extends Base {
 	public static int staticInt = 10;
 	public static double staticDouble = 2.71;
 	public static boolean staticBool = false;
 	public static String staticNullString = null;
 	public static final int staticFinalInt = 99;
 	public static Integer staticWrapper = 50;
+}
+
+class Child extends Base {
 }
